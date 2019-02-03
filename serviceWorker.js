19ec2bdd9fa672ea46765fde6d6ec9b0a7b8858c
rivalmsr <rivalmusripal@gmail.com>
2019-02-03 09:47:37 +0700

    revisi-service-worker-v2

--- serviceWorker.js
+++ serviceWorker.js
@@ -19,39 +19,56 @@ self.addEventListener('install', function(event){
     );
 });
 
+//custome fetch
 self.addEventListener('fetch', function(event){
-    var request = event.request
-    var url = new URL(request.url)
+
+  var request = event.request
+  var url = new URL (request.url)
 
 
-    //pisahkan request API dan Internal
-    if(url.origin === location.origin){
-        event.respondWith(
-            caches.match(request).then(function(response){
-                return response || fetch(request)
-            })
-        )
-    }else{
-        event.respondWith(
-            caches.open('products-cache').then(function(cache){
-                return fetch(request).then(function(liveResponse){
-                    cache.put(request, liveResponse.close())
-                    return liveResponse
-                }).catch(function(){
-                    return caches.match(request).then(function(response){
-                        if(response) return response
-                        return caches.match('/fallback.json')
-                    })
-                })
-            })
-        )
-    }
+  //memisahkan request API dan Internal
+  if(url.origin === location.origin){
+    event.respondWith(
+      caches.match(request).then(function(response){
+        return response || fetch(request)
+      })
+    )
+  }else{
+    event.respondWith(
+      caches.open('products-cache').then(function(cache){
+        return fetch(request).then(function(liveResponse){
+          cache.put(request, liveResponse.clone())
+          console.log(liveResponse);
+          return liveResponse
+        }).catch(function(){
+          return caches.match(request).then(function(response){
+            if(response) return response
+            return caches.match('/fallback.json')
+          })
+        })
+      })
+    )
+  }
+});
+
+//default fetch
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      if(response){
+        return response;
+      }
+      return fetch(event.request);
+  )
+});
+
+
 })
 
 // Update a service worker
 self.addEventListener('activate', function(event){
-    event.waitUntil(    
-        caches.keys().then(function(cacheName){
+    event.waitUntil(
+        caches.keys().then(function(cacheNames){
             return Promise.all(
                 cacheNames.filter(function(cacheName){
                     return cacheName != CACHE_NAME
