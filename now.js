524987df03e7b486731a734035b379d2c5343584
Rival Musripal <rivalmusripal@gmail.com>
2019-02-04 11:37:59 +0700

    Update now.js

--- now.js
+++ now.js
@@ -1,31 +1,3 @@
 {
-    "name": "monorepo",
-    "version": 2,
-    "builds": [
-        { "src": "www/package.json", "use": "@now/next" },
-        { "src": "api/go/*.go", "use": "@now/go" },
-        { "src": "api/python/*.py", "use": "@now/python" },
-        { "src": "api/php/*.php", "use": "@now/php" },
-        { "src": "api/node/*.js", "use": "@now/node" }
-    ],
-    "routes": [
-        {
-          "src": "/api/(.*)",
-          "dest": "/api/$1"
-        },
-        {
-          "src": "/redirect-test",
-          "status": 302,
-          "headers": {
-            "location": "https://google.com"
-          }
-        },
-        {
-          "src": "/(.*)",
-          "dest": "/www/$1",
-          "headers": {
-            "x-request-path": "$1"
-          }
-        }
-    ]
+  "alias": ["my-zeit-website.com"]
 }
